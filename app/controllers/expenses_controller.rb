f9d592d7f637791deeea8de1443330f92c1266ba
Kavita <jadhavkavita206@gmail.com>
2014-06-05 15:29:56 +0530

    showing total expense

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,6 +1,7 @@
 class ExpensesController < ApplicationController
+
   def index
     @expenses = Expense.all
-    #@expenses = [{:name => "Milk" ,:amount => 100}, {:name => "Movie" ,:amount => 200}, {:name => "Novel" ,:amount => 500}]
   end
+
 end

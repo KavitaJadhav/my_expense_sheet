a9e280d6fb5e09ef7edf58d3900f215ebc8708c2
Kavita <jadhavkavita206@gmail.com>
2014-06-05 16:57:06 +0530

    adding new expences in expence sheet

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,7 +1,21 @@
 class ExpensesController < ApplicationController
 
   def index
+
     @expenses = Expense.all
   end
 
+  def add_expense
+    @new_expense = Expense.new
+  end
+
+  def create_expense
+    expense = Expense.create(:name => params[:expense][:name],:amount => params[:expense][:amount])
+    unless expense.valid?
+      flash[:error] = expense.errors.full_messages.join("<br>").html_safe
+    else
+      flash[:success] = "#{expense[:name]}' added successfully"
+    end
+    redirect_to index_path
+  end
 end

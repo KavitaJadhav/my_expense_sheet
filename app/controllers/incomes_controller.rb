1337c382bf905bdae02947b0aafd55f51ab735db
Kavita <jadhavkavita206@gmail.com>
2014-06-05 17:51:47 +0530

    Adding income and showing total income

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -2,4 +2,18 @@ class IncomesController < ApplicationController
   def index
     @incomes = Income.all
   end
+
+  def add_income
+    @new_income = Income.new
+  end
+
+  def create_income
+    income = Income.create(:source => params[:income][:source],:amount => params[:income][:amount])
+    unless income.valid?
+      flash[:error] = income.errors.full_messages.join("<br>").html_safe
+    else
+      flash[:success] = "#{income[:name]}' added successfully"
+    end
+    redirect_to inc_index_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 MyExpenseSheet::Application.routes.draw do
-  get "incomes/index"
+  #get "incomes/index"
   #get "expenses/index"
 
   match "expenses/index" => "expenses#index", :as => :index, via: [:get, :post]
+  match "incomes/index" => "incomes#index", :as => :inc_index, via: [:get, :post]
   get "expenses/add_expense" => "expenses#add_expense"
   post "expenses/add_expense" => "expenses#create_expense"
 
-  #match "incomes/index" => "incomes#index", :as => :index, via: [:get, :post]
+  get "incomes/add_income" => "incomes#add_income"
+  post "incomes/add_income" => "incomes#create_income"
+
 end

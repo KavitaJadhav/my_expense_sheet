e87157dcac5cbe5b3d678da086c80a70cc5353cc
Kavita <jadhavkavita206@gmail.com>
2014-06-05 12:51:15 +0530

    New rails project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyExpenseSheet::Application.config.secret_key_base = '44b820e3fb4e7934131081ed6c75ec4c0f8dabb3f7cf5eab6bb70ad5cd2d9e51cf5feaf3272bdcb134de80fcb773afae773cb17f4fa34e0294b3fe4a9a35b2e6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyExpenseSheet::Application.config.session_store :cookie_store, key: '_my_expense_sheet_session'

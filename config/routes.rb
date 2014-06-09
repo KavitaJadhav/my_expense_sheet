ab39272e567e21ee6b1991771ce1b0bb5a5a5bcf
Kavita <jadhavkavita206@gmail.com>
2014-06-09 17:34:02 +0530

    refactered,route added for reset action

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 MyExpenseSheet::Application.routes.draw do
-  #get "incomes/index"
-  #get "expenses/index"
+  #get 'incomes/index'
+  #get 'expenses/index'
 
-  match "expenses" => "expenses#index", :as => :expense_index, via: [:get, :post]
-  match "incomes" => "incomes#index", :as => :income_index, via: [:get, :post]
-  get "expenses/add_expense" => "expenses#add_expense"
-  post "expenses/add_expense" => "expenses#create_expense"
+  match 'expenses' => 'expenses#index', :as => :expense_index, via: [:get, :post]
+  match 'incomes' => 'incomes#index', :as => :income_index, via: [:get, :post]
+  get 'expenses/add_expense' => 'expenses#add_expense'
+  post 'expenses/add_expense' => 'expenses#create_expense'
 
-  get "incomes/add_income" => "incomes#add_income"
-  post "incomes/add_income" => "incomes#create_income"
-
+  get 'incomes/add_income' => 'incomes#add_income'
+  post 'incomes/add_income' => 'incomes#create_income'
+  
+  get 'reset/delete_all' => 'reset#delete_all'
 end

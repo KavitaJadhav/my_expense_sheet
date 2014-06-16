4f83357b4573ebe91bc6433e006397aea65b8812
Kavita <jadhavkavita206@gmail.com>
2014-06-16 16:09:35 +0530

    gemfile modified

---
+++ vendor/bundle/ruby/2.0.0/gems/json-1.8.1/ext/json/ext/parser/siteconf20140610-23788-z25tr5.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140610-23788-1rcciat"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path

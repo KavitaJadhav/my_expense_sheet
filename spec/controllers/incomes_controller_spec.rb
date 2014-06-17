561001c8cad789cdb6ba497d41c50fcc6516fef9
Kavita <jadhavkavita206@gmail.com>
2014-06-17 12:10:15 +0530

    testing income not added with nil vlues

--- spec/controllers/incomes_controller_spec.rb
+++ spec/controllers/incomes_controller_spec.rb
@@ -4,17 +4,31 @@ describe IncomesController do
 
   describe 'create_income' do
 
-    it 'should create an income' do
+    it 'should create an income with valid values' do
 
       post :create_income, income: {source: "salary", amount: 100}
 
       income = Income.last
 
-      Income.count.should == 1
-      income.source.should == "salary"
-      income.amount.should == 100
+      response.response_code.should == 302
+
+      expect(Income.count).to eq 1
+      expect(income.source).to eq "salary"
+      expect(income.amount).to eq 100
 
     end
+
+
+    it 'should not create an income with invalid values' do
+
+      post :create_income, income: {source: nil, amount: nil}
+
+      #expect(response).not_to be_success
+
+      expect(Income.count).to eq 0
+
+    end
+
   end
 
 end

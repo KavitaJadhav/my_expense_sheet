60f1742c335d890a27ec7522e2ea37e42b8350df
Kavita <jadhavkavita206@gmail.com>
2014-06-17 12:34:40 +0530

    asserting status code for adding income

--- spec/controllers/incomes_controller_spec.rb
+++ spec/controllers/incomes_controller_spec.rb
@@ -5,28 +5,22 @@ describe IncomesController do
   describe 'create_income' do
 
     it 'should create an income with valid values' do
-
+      income_count_before_post_request = Income.count
       post :create_income, income: {source: "salary", amount: 100}
 
       income = Income.last
 
       response.response_code.should == 302
-
-      expect(Income.count).to eq 1
+      expect(Income.count).to eq income_count_before_post_request+1
       expect(income.source).to eq "salary"
       expect(income.amount).to eq 100
-
     end
 
 
     it 'should not create an income with invalid values' do
-
+      income_count_before_post_request = Income.count
       post :create_income, income: {source: nil, amount: nil}
-
-      #expect(response).not_to be_success
-
-      expect(Income.count).to eq 0
-
+      expect(Income.count).to eq income_count_before_post_request
     end
 
   end

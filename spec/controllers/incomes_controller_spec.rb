3d6f23b5e026d1ae5cac14c12da86f329a3fd7cc
Kavita <jadhavkavita206@gmail.com>
2014-06-17 12:53:52 +0530

    asserting for error and succes messages for create income

--- spec/controllers/incomes_controller_spec.rb
+++ spec/controllers/incomes_controller_spec.rb
@@ -14,6 +14,7 @@ describe IncomesController do
       expect(Income.count).to eq income_count_before_post_request+1
       expect(income.source).to eq "salary"
       expect(income.amount).to eq 100
+      expect(flash[:success]).to eq "salary added to Incomes successfully"
     end
 
 
@@ -21,8 +22,16 @@ describe IncomesController do
       income_count_before_post_request = Income.count
       post :create_income, income: {source: nil, amount: nil}
       expect(Income.count).to eq income_count_before_post_request
+      expect(flash[:error]).to eq "Source can't be blank<br>Amount can't be blank"
     end
 
+  end
+
+  describe 'add_income' do
+    it 'should redirect to add income page' do
+      get :add_income
+      response.response_code.should == 200
+    end
   end
 
 end
